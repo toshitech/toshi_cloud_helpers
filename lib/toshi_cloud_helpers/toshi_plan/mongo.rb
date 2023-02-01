ef8b978a615baaa23694fa95c745b008e533be68
Chris Hopkins <42541529+chris-john-hopkins@users.noreply.github.com>
2023-02-01 17:40:53 +0000

    add update_one method

--- lib/toshi_cloud_helpers/toshi_plan/mongo.rb
+++ lib/toshi_cloud_helpers/toshi_plan/mongo.rb
@@ -6,11 +6,15 @@ module ToshiCloudHelpers
   module ToshiPlan
     class Mongo
       def initialize
-        @db_connection = ::Mongo::Client.new([ENV['MONGO_HOST']], database_config)
+        @db_connection = ::Mongo::Client.new(ENV['MONGO_HOST'], database_config)
       end
 
       def perform(collection_name)
         @db_connection[collection_name]
+      end
+
+      def update_one(collection_name, filter, update)
+        @db_connection[collection_name].find(filter).update_one(update)
       end
 
       protected
