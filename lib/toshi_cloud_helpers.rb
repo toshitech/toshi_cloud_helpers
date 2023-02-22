161f5e012e4a377008cccb02785ef0082055a7c8
Chris Hopkins <42541529+chris-john-hopkins@users.noreply.github.com>
2023-02-22 16:37:08 +0000

    require api jobs

--- lib/toshi_cloud_helpers.rb
+++ lib/toshi_cloud_helpers.rb
@@ -6,6 +6,8 @@ require "pg"
 require_relative "toshi_cloud_helpers/version"
 require_relative "toshi_cloud_helpers/toshi_web/postgres"
 require_relative "toshi_cloud_helpers/toshi_web/database_row"
+require_relative "toshi_cloud_helpers/toshi_web/api/base"
+require_relative "toshi_cloud_helpers/toshi_web/api/jobs"
 require_relative "toshi_cloud_helpers/toshi_plan/mongo/base"
 require_relative "toshi_cloud_helpers/toshi_plan/mongo/shipments"
 
